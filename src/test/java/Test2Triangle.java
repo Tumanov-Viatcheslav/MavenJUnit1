5b770c0ee5352272d897ad0891caca41b832c782
Tumanov <tslavich@bk.ru>
2024-10-02 14:31:33 +0300

    Test constructor

--- src/test/java/Test2Triangle.java
+++ src/test/java/Test2Triangle.java
@@ -1,7 +1,6 @@
 import mydomain.geometry.Point;
 import mydomain.geometry.Triangle;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class Test2Triangle {
@@ -47,5 +46,32 @@ public class Test2Triangle {
                 p3 = new Point(4, 120);
         Triangle t1 = new Triangle(p1, p2, p3);
         Assertions.assertTrue(Math.abs(t1.perimeter() - p1.distanceTo(p2) - p2.distanceTo(p3) - p3.distanceTo(p1)) < 0.0001);
+    }
+
+    @Test
+    public void testPerimeter5(){
+        Point p1 = new Point(0, 0),
+                p2 = new Point(0, 0),
+                p3 = new Point(0, 0);
+        Point cp1 = new Point(1, 1),
+                cp2 = new Point(3, 3),
+                cp3 = new Point(4, 120);
+        IllegalArgumentException ex = Assertions.assertThrows(IllegalArgumentException.class, () -> new Triangle(p1, p2, p3));
+        Assertions.assertEquals("Equal points in triangle", ex.getMessage());
+        ex = Assertions.assertThrows(IllegalArgumentException.class, () -> new Triangle(p1, cp2, p3));
+        Assertions.assertEquals("Equal points in triangle", ex.getMessage());
+        ex = Assertions.assertThrows(IllegalArgumentException.class, () -> new Triangle(cp1, p2, p3));
+        Assertions.assertEquals("Equal points in triangle", ex.getMessage());
+        ex = Assertions.assertThrows(IllegalArgumentException.class, () -> new Triangle(p1, p2, cp3));
+        Assertions.assertEquals("Equal points in triangle", ex.getMessage());
+    }
+
+    @Test
+    public void testPerimeter6(){
+        Point p1 = new Point(0, 0),
+                p2 = new Point(0, 30),
+                p3 = new Point(40, 0);
+        Triangle t1 = new Triangle(p1, p2, p3);
+        Assertions.assertEquals(120, t1.perimeter());
     }
 }
